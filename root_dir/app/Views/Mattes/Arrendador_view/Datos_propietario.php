cc255cbd66b8546f0a5e5f1551a0a6261abb2496
Jesús Sánchez <jesestsana93@hotmail.com>
2026-03-30 17:28:53 -0600

    banner de cuenta

--- root_dir/app/Views/Mattes/Arrendador_view/Datos_propietario.php
+++ root_dir/app/Views/Mattes/Arrendador_view/Datos_propietario.php
@@ -25,7 +25,7 @@
     </div>
 </div> --> 
 
-<section class="section-sub-banner bg-9">
+<section class="section-sub-banner bg-cuenta">
     <div class="sub-banner">
         <div class="container">
             <div class="text text-center">
